2c03bc7adbf1a3e6d51171587eec2bed4cc62eb7
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-20 08:15:02 -0300

    fix: add padding in score_card_widget

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -16,31 +16,34 @@ class ScoreCardWidget extends StatelessWidget {
           color: AppColors.white,
           borderRadius: BorderRadius.circular(15)
         ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 1,
-              child: ChartWidget(),          
-            ),
-            Expanded(
-              flex: 3,
-              child: Padding(
-                  padding: const EdgeInsets.only(left: 24),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-
-                  children: [
-                    Text("Vamos começar", style: AppTextStyles.heading),
-                    Text("Complete os desafios e avance em conhecimento", 
-                      style: AppTextStyles.body,
-                    )
-                  ],
-                ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: ChartWidget(),          
               ),
-            )
-          ],
+              Expanded(
+                flex: 3,
+                child: Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                    children: [
+                      Text("Vamos começar", style: AppTextStyles.heading),
+                      Text("Complete os desafios e avance em conhecimento", 
+                        style: AppTextStyles.body,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
