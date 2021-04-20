73f4a77b7d35bf046ae33dfe6e832c172cdfd46c
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-20 09:54:52 -0300

    feat: add quiz_card_widget

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,21 +16,26 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBarWidget(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row (
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            LevelButtonWidget(
-              label: "Fácil",
+            Row (
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(
+                  label: "Fácil",
+                ),
+                 LevelButtonWidget(
+                  label: "Médio",
+                ),
+                 LevelButtonWidget(
+                  label: "Difícil",
+                ),
+                 LevelButtonWidget(
+                  label: "Perito",
+                )
+              ],
             ),
-             LevelButtonWidget(
-              label: "Médio",
-            ),
-             LevelButtonWidget(
-              label: "Difícil",
-            ),
-             LevelButtonWidget(
-              label: "Perito",
-            )
+            QuizCardWidget()
           ],
         ),
       )
