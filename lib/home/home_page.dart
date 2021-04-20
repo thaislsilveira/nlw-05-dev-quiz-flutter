17f0fa2e096df89bf6c01c14823952be4c1638b9
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-20 08:48:51 -0300

    feat: add level

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +12,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget()
+      appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row (
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LevelButtonWidget(
+              label: "Fácil",
+            ),
+             LevelButtonWidget(
+              label: "Médio",
+            ),
+             LevelButtonWidget(
+              label: "Difícil",
+            ),
+             LevelButtonWidget(
+              label: "Perito",
+            )
+          ],
+        ),
+      )
     );
   }
 }
