43ed8b50a4b63842d3e9ee14a384e0373111281a
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-21 11:35:10 -0300

    feat: add HomeRepository

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/core/app_images.dart';
+import 'package:DevQuiz/home/home_repository.dart';
 import 'package:DevQuiz/home/home_state.dart';
 import 'package:DevQuiz/shared/models/answer_model.dart';
 import 'package:DevQuiz/shared/models/question_model.dart';
@@ -15,42 +16,17 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-
-    user = UserModel(
-      name: "Thaís",
-      photoUrl: "https://avatars.githubusercontent.com/u/34002389?v=4",
-    );
+    user = await repository.getUser();
     state = HomeState.success;
   }
 
   void getQuizzes() async {
-    state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    quizzes = [
-      QuizModel(
-          title: "NLW 5 Flutter",
-          imagem: AppImages.blocks,
-          questionAnswered: 1,
-          level: Level.facil,
-          questions: [
-            QuestionModel(title: "Está curtindo o Flutter?", answers: [
-              AnswerModel(title: "Estou curtindo"),
-              AnswerModel(title: "Amando o Flutter"),
-              AnswerModel(title: "Muito Top"),
-              AnswerModel(title: "Show de bola", isRight: true)
-            ]),
-            QuestionModel(title: "Está curtindo o Flutter?", answers: [
-              AnswerModel(title: "Estou curtindo"),
-              AnswerModel(title: "Amando o Flutter"),
-              AnswerModel(title: "Muito Top"),
-              AnswerModel(title: "Show de bola", isRight: true)
-            ])
-          ])
-    ];
-
+    state = HomeState.loading;  
+    quizzes = await repository.getQuizzes();
     state = HomeState.success;
   }
 }
