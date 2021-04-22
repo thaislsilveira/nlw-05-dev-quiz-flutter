40c72f705d213f8f34af092ccb2695a52ae59849
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-22 14:13:28 -0300

    feat: add challenge_controller

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/challenge/challenge_controller.dart';
 import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
@@ -13,7 +14,16 @@ class ChallengePage extends StatefulWidget {
 }
 
 class _ChallengePageState extends State<ChallengePage> {
+  final controller = ChallengeController();
+  final pageController = PageController();
   @override
+  void initState() {
+    pageController.addListener(() {
+      controller.currentPage = pageController.page!.toInt() + 1;
+    });
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
@@ -29,13 +39,21 @@ class _ChallengePageState extends State<ChallengePage> {
                   Navigator.pop(context);
                 },
               ),
-              QuestionIndicatorWidget(),
+              ValueListenableBuilder<int>(
+                valueListenable: controller.currentPageNotifier,
+                builder: (context, value, _) => QuestionIndicatorWidget(
+                  currentPage: value,
+                  length: widget.questions.length,
+                ),
+              )
             ],
           ),
         ),
       ),
-      body: QuizWidget(
-       question: widget.questions[0],
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: widget.questions.map((e) => QuizWidget(question: e)).toList(),
       ),
       bottomNavigationBar: SafeArea(
         bottom: true,
@@ -46,7 +64,12 @@ class _ChallengePageState extends State<ChallengePage> {
             Expanded(
                 child: NextButtonWidget.white(
               label: "Pular",
-              onTap: () {},
+              onTap: () {
+                pageController.nextPage(
+                  duration: Duration(milliseconds: 100),
+                  curve: Curves.linear,
+                );
+              },
             )),
             SizedBox(width: 7),
             Expanded(
--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -3,33 +3,32 @@ import 'package:DevQuiz/home/widgets/progress_indicator/progress_indicator_widge
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
-  const QuestionIndicatorWidget({Key? key}) : super(key: key);
+  final int currentPage;
+  final int length;
+  const QuestionIndicatorWidget({Key? key, required this.currentPage, required this.length}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: [
-         Row(
-           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-           children: [
-            Text("Questão 04", 
-              style: AppTextStyles.body,
-            ),
-            Text("de 10",
-              style: AppTextStyles.body,
-            )
-           ]
-         ),
-         SizedBox(
-           height: 16,
-         ),
-         ProgressIndicatorWidget(
-           value: 0.7,
-         )
-        ]
-      ),
+      child: Column(children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(
+            "Questão $currentPage",
+            style: AppTextStyles.body,
+          ),
+          Text(
+            "de $length",
+            style: AppTextStyles.body,
+          )
+        ]),
+        SizedBox(
+          height: 16,
+        ),
+        ProgressIndicatorWidget(
+          value: currentPage/ length,
+        )
+      ]),
     );
   }
 }
