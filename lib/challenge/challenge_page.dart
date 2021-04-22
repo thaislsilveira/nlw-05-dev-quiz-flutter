8a062e8f24a5a0f1fd3afc8e4f9f4fea2c994a9a
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-22 08:47:50 -0300

    feat: add next_button_widget

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,20 @@ class _ChallengePageState extends State<ChallengePage> {
       ),
       body: QuizWidget(
         title: "O que o Flutter faz em sua totalidade?"
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(child: NextButtonWidget.white(label: "Pular", onTap: (){}, )),
+              SizedBox(width: 7),
+              Expanded(child: NextButtonWidget.green(label: "Confirmar", onTap: (){}, )),
+            ]
+          ),
+        ),
       ),
     );
   }
