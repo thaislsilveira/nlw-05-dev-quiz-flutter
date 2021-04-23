64a82c73b67d76e1738ef0749cde8fb607654da7
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-23 08:23:05 -0300

    fix: add navigation Splash for Home

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,4 +1,4 @@
-import 'package:DevQuiz/home/home_page.dart';
+import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -6,7 +6,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
