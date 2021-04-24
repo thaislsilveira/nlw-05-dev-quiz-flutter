e2b16ceb3fd1deca8bc86ba8f311b0ecfec1da04
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-24 11:01:29 -0300

    feat: add share

--- lib/result/result_page.dart
+++ lib/result/result_page.dart
@@ -2,13 +2,18 @@ import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:DevQuiz/core/app_images.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ResultPage extends StatelessWidget {
   final String title;
   final int length;
   final int result;
 
-  const ResultPage({Key? key, required this.title, required this.length, required this.result})
+  const ResultPage(
+      {Key? key,
+      required this.title,
+      required this.length,
+      required this.result})
       : super(key: key);
 
   @override
@@ -48,7 +53,11 @@ class ResultPage extends StatelessWidget {
                       child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 68),
                     child: NextButtonWidget.purple(
-                        label: "Compartilhar", onTap: () {}),
+                        label: "Compartilhar",
+                        onTap: () {
+                          Share.share(
+                              'DevQuiz NLW 5 - Flutter: Resultado do Quiz : $title\nObtive: ${result/length}% aproveitamento!');
+                        }),
                   )),
                 ],
               ),
